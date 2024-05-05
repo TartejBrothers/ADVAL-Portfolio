9f25bde673a78162611894c87fc7b09cd38fbc96
TartejBrothers <taranjeetsinghbedi2@gmail.com>
2024-05-05 23:11:27 +0530

    Updated: Designs

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import Home from "./components/home";
 import Expertise from "./components/expertise";
 import Showcase from "./components/showcase";
+import Designs from "./components/designs";
 function App() {
   return (
     <div className="App">
       <Home />
       <Expertise />
       <Showcase />
+      <Designs />
     </div>
   );
 }
